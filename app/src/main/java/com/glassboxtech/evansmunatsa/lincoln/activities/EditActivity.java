fcf42f60c0e4b974d644da31e841fa58e9d503ce
Evans-Munatsa <evansmunatsa7@gmail.com>
2019-03-14 07:09:20 +0200

    inhancements on what is the future of the app

---
+++ app/src/main/java/com/glassboxtech/evansmunatsa/lincoln/activities/EditActivity.java
@@ -0,0 +1,15 @@
+package com.glassboxtech.evansmunatsa.lincoln.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.glassboxtech.evansmunatsa.lincoln.R;
+
+public class EditActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit);
+    }
+}
